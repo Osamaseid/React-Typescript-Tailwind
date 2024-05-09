44d92b700c9b7cc92e6b47fc92fda10fcbbbeddd
OSAMA SEID <119799518+Osamaseid@users.noreply.github.com>
2024-05-09 09:48:09 +0300

    Add files via upload
    State and life cycle in React

---
+++ State-life-cycle/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './Counter'
+import Timer from './Timer'
+
+function App() {
+  return (
+    <>
+    <Timer/>
+    <Counter/>
+    </>
+  )
+}
+
+export default App

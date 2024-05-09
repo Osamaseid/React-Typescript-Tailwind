0338643a473301272f16a1a528a643ab4d8ce692
OSAMA SEID <119799518+Osamaseid@users.noreply.github.com>
2024-05-09 10:43:24 +0300

    Add files via upload
    State-Life cycle

---
+++ State-life-cycle/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CounterApp from './Components/Counter'
+import Timer from './Timer'
+
+function App() {
+  return (
+    <>
+    <Timer/>
+    <CounterApp/>
+    </>
+  )
+}
+
+export default App
---
+++ State-life-cycle/src/Components/Counter.ts
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>s
+  );
+}
+
+export default CounterApp;

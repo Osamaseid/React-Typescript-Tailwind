a236c2d931b92f477af3f27ada340e790012caf1
OSAMA SEID <119799518+Osamaseid@users.noreply.github.com>
2024-05-09 10:41:57 +0300

    Delete fun-class-comp directory

--- fun-class-comp/src/Components/Child-comp.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-function ChildComponent(props) {
-  return (
-  <h1>Hello, {props.name} {props.id}!</h1>
-  );
-}
-
-export default ChildComponent;
--- fun-class-comp/src/Components/Functional-comp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function FunctionalComponent() {
-  return (
-  <h1>This is a functional component</h1>
-  );
-}
-
-export default FunctionalComponent;

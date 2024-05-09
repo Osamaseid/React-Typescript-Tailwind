59ed96b62a65d45e80514b862cb52eacf3534252
OSAMA SEID <119799518+Osamaseid@users.noreply.github.com>
2024-05-09 09:49:39 +0300

    Add files via upload
    To-Do APP by suing React, Typescript and Tailwindcss

---
+++ Todo-project/src/App.tsx
@@ -0,0 +1,30 @@
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import useTodos from "./hooks/useTodos";
+
+function App() {
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+   
+  } = useTodos();
+
+  return (
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center">Todo-App</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
+      </div>
+     
+    </main>
+  );
+}
+
+export default App;
---
+++ Todo-project/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "React ",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Javascript",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "TailwindCss",
+    completed: false,
+  },
+];
